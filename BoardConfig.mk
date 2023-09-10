3cc4a1d912242a6be42ce90ee91ba1c2fd8ba34f
Kousick Dey <kousickdey3@gmail.com>
2023-09-10 17:30:48 +0530

    RMX2020: Move Maintainer info to prop
    Signed-off-by: Kousick Dey <kousickdey3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,6 +162,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6768
